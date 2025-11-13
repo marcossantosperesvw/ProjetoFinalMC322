f64c3af901aafa26aed93d479641094ffe8ca261
Felipe Costa Amaral <ra249239@beach.lab.ic.unicamp.br>
2025-11-12 21:08:12 -0300

    criação das peças, não finalizado

--- app/src/main/java/org/chess/board/Board.java
+++ app/src/main/java/org/chess/board/Board.java
@@ -7,6 +7,7 @@ import com.google.common.graph.MutableGraph;
 
 import org.chess.Move;
 import org.chess.Pos;
+import org.chess.Clock;
 import org.chess.Color;
 import org.chess.King;
 import org.chess.Piece;
@@ -14,6 +15,7 @@ import org.chess.Player;
 
 import java.util.EnumMap;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class Board {
@@ -23,16 +25,207 @@ public class Board {
   private final Map<Color, Player> players = new EnumMap<>(Color.class);
   public final History history = new History();
 
-   
+  // String[] board = {
+  //   "r##RkBKQBkR###",
+  //   "###PPPPPPPP###",
+  //   "###@@@@@@@@###",
+  //   "RP@@@@@@@@@@PR",
+  //   "kP@@@@@@@@@@Pk",
+  //   "BP@@@@@@@@@@PB",
+  //   "KP@@@@@@@@@@PQ",
+  //   "QP@@@@@@@@@@PK",
+  //   "BP@@@@@@@@@@PB",
+  //   "kP@@@@@@@@@@Pk",
+  //   "RP@@@@@@@@@@PR",
+  //   "###@@@@@@@@###",
+  //   "###PPPPPPPP###",
+  //   "###RkBQKBkR###"
+  // };
+
   public Board() {
-    // initialize all data structures above
-    // create a temporary list of pieces
-    // create and add pieces to pieces map and to the temporary list.
-    // create a player with that list of pieces
-    // add player to platers.
-    // repeat for every player
-    //
-    // reavaluate all pieces.
+    Piece piece;
+    Pos pos;
+    King king;
+
+    // Red player is the top one.
+    List<Piece> redPieces = new ArrayList<>();
+    king = new King();
+    redPieces.add(king);
+    players.put(Color.RED, new Player(redPieces, king, new Clock(MAX_TIME), Color.RED));
+
+    piece = new Tower();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 4), piece);
+
+    piece = new Knight();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 5), piece);
+
+    piece = new Bishop();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 6), piece);
+
+    piece = new Queen();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 7), piece);
+
+    redPieces.add(king);
+    pieces.put(new Pos(1, 8), king);
+
+    piece = new Bishop();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 9), piece);
+
+    piece = new Knight();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 10), piece);
+
+    piece = new Tower();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 11), piece);
+
+    for (int i = 4; i <= 14; i++) {
+      piece = new Pawn();
+      redPieces.add(piece);
+      pieces.put(new Pos(2, i), piece);
+    }
+
+    // Yellow player is the bottom one
+    List<Piece> yellowPieces = new ArrayList<>();
+    king = new King();
+    redPieces.add(king);
+    players.put(Color.RED, new Player(yellowPieces, king, new Clock(MAX_TIME), Color.RED));
+
+    piece = new Tower();
+    yellowPieces.add(piece);
+    pieces.put(new Pos(14, 4), piece);
+
+    piece = new Knight();
+    yellowPieces.add(piece);
+    pieces.put(new Pos(14, 5), piece);
+
+    piece = new Bishop();
+    yellowPieces.add(piece);
+    pieces.put(new Pos(14, 6), piece);
+
+    yellowPieces.add(king);
+    pieces.put(new Pos(14, 7), king);
+
+    piece = new Queen();
+    yellowPieces.add(piece);
+    pieces.put(new Pos(14, 8), piece);
+
+    piece = new Bishop();
+    yellowPieces.add(piece);
+    pieces.put(new Pos(14, 9), piece);
+
+    piece = new Knight();
+    yellowPieces.add(piece);
+    pieces.put(new Pos(14, 10), piece);
+
+    piece = new Tower();
+    yellowPieces.add(piece);
+    pieces.put(new Pos(14, 11), piece);
+
+    for (int i = 4; i <= 14; i++) {
+      piece = new Pawn();
+      yellowPieces.add(piece);
+      pieces.put(new Pos(13, i), piece);
+    }
+
+    List<Piece> redPieces = new ArrayList<>();
+    king = new King();
+    redPieces.add(king);
+    players.put(Color.RED, new Player(redPieces, king, new Clock(MAX_TIME), Color.RED));
+
+    piece = new Tower();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 4), piece);
+
+    piece = new Knight();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 5), piece);
+
+    piece = new Bishop();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 6), piece);
+
+    piece = new Queen();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 7), piece);
+
+    redPieces.add(king);
+    pieces.put(new Pos(1, 8), king);
+
+    piece = new Bishop();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 9), piece);
+
+    piece = new Knight();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 10), piece);
+
+    piece = new Tower();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 11), piece);
+
+    for (int i = 4; i <= 14; i++) {
+      piece = new Pawn();
+      redPieces.add(piece);
+      pieces.put(new Pos(2, i), piece);
+    }
+
+    List<Piece> redPieces = new ArrayList<>();
+    king = new King();
+    redPieces.add(king);
+    players.put(Color.RED, new Player(redPieces, king, new Clock(MAX_TIME), Color.RED));
+
+    piece = new Tower();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 4), piece);
+
+    piece = new Knight();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 5), piece);
+
+    piece = new Bishop();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 6), piece);
+
+    piece = new Queen();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 7), piece);
+
+    redPieces.add(king);
+    pieces.put(new Pos(1, 8), king);
+
+    piece = new Bishop();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 9), piece);
+
+    piece = new Knight();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 10), piece);
+
+    piece = new Tower();
+    redPieces.add(piece);
+    pieces.put(new Pos(1, 11), piece);
+
+    for (int i = 4; i <= 14; i++) {
+      piece = new Pawn();
+      redPieces.add(piece);
+      pieces.put(new Pos(2, i), piece);
+    }
+
+  }
+
+  // create a temporary list of pieces
+  // create and add pieces to pieces map and to the temporary list.
+  // create a player with that list of pieces
+  // add player to platers.
+  // repeat for every player
+  //
+  // reavaluate all pieces.
   }
 
   public int getNOfPlayers() {
@@ -47,9 +240,8 @@ public class Board {
   public Pos getPiece(Pos pos) {
   }
 
-
   public List<King> getEndangeredKings() {
-   }
+  }
 
   public void doMove(Move move) {
     // check player clock to see if it still has time.
@@ -77,17 +269,15 @@ public class Board {
     // use piece.calculateMoves() to get movesList and blockingPieces
     // in reevaluationGraph, add edges from the blockingPieces to piece
     // moves.submiMove(move)
-    }
-
+  }
 
   private void removePlayer(Color color) {
     // get player instance
     // get list of pieces
     // remove them from pieces BiMap
-    // remove them from graph, but store, in a temp variable, all pieces poited by them.
+    // remove them from graph, but store, in a temp variable, all pieces poited by
+    // them.
     // reevaluate all pieces in the temp variable
   }
 
-
-  
 }
