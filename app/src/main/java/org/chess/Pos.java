1c48dac9f3a30b94d82cfb81255e28edc6d02b8e
LipeAma <contato.feamaral@gmail.com>
2025-11-15 18:25:27 -0300

    Pos rotation

--- app/src/main/java/org/chess/Pos.java
+++ app/src/main/java/org/chess/Pos.java
@@ -5,11 +5,7 @@ import java.util.List;
 
 public record Pos(int row, int column) {
   public Pos {
-    if (row < 1
-        || column < 1
-        || row > 14
-        || column > 14
-        || (row < 4 || row > 11) && (column < 4 || column > 11)) {
+    if (row < 1 || column < 1 || row > 14 || column > 14 || (row < 4 || row > 11) && (column < 4 || column > 11)) {
       throw new IllegalArgumentException("Invalid position");
     }
   }
@@ -26,4 +22,70 @@ public record Pos(int row, int column) {
     }
     return list;
   }
+
+  /**
+   * Transforms a position from the board's perspective to the equivalent position
+   * from the color's perspective.
+   *
+   * Board Layout Context:
+   * The board is a 14x14 grid with a 3x3 square removed from each of the four
+   * corners.
+   * The sides are associated with player colors as follows:
+   * - RED: Top side (Row 1)
+   * - YELLOW: Left side (Column 1)
+   * - BLUE: Right side (Column 14)
+   * - GREEN: Bottom side (Row 14)
+   *
+   * @param color whose perspective is being converted to.
+   * @return The transformed position as if color were at the bottom (Row 14).
+   */
+  public Pos relativeTo(Color color) {
+    return rotateClockwise(
+        switch (color) {
+          case RED -> 2;
+          case YELLOW -> 3;
+          case GREEN -> 0;
+          case BLUE -> 1;
+          default -> throw new IllegalStateException("Unexpected enum.");
+        });
+  }
+
+  /**
+   * Transforms a position from the color's perspective to the equivalent position
+   * from the board's perspective.
+   *
+   * Board Layout Context:
+   * The board is a 14x14 grid with a 3x3 square removed from each of the four
+   * corners.
+   * The sides are associated with player colors as follows:
+   * - RED: Top side (Row 1)
+   * - YELLOW: Left side (Column 1)
+   * - BLUE: Right side (Column 14)
+   * - GREEN: Bottom side (Row 14)
+   *
+   * @param color The color whose perspective is being converted from.
+   * @return The transformed position form the board's perpective.
+   */
+  public Pos fromPerspective(Color color) {
+    return rotateClockwise(
+        switch (color) {
+          case RED -> 2;
+          case YELLOW -> 1;
+          case GREEN -> 0;
+          case BLUE -> 3;
+          default -> throw new IllegalStateException("Unexpected enum.");
+        });
+  }
+
+  private Pos rotateClockwise(int nOfRotations) {
+    nOfRotations %= 4;
+    return switch (nOfRotations) {
+      case 0 -> this;
+      case 1 -> new Pos(15 - this.row, this.column);
+      case 2 -> new Pos(15 - this.row, 15 - this.column);
+      case 3 -> new Pos(15 - this.column, this.row);
+      default -> throw new IllegalStateException("Unexpected remainder.");
+    };
+  }
+
 }
